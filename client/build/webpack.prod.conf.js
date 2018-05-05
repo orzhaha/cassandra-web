a900d85a5af22e5a2cb1278103ec607f99648fca
Unknown <s811214studen@gmail.com>
2018-05-05 23:05:50 +0800

    What: dist

--- client/build/webpack.prod.conf.js
+++ client/build/webpack.prod.conf.js
@@ -26,8 +26,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].js?[chunkhash]'),
+    chunkFilename: utils.assetsPath('js/[id].js?[chunkhash]')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
