c13ffea02aa4f2be3d81a3a43a507b5298a7264b
Ken <kfrico@gmail.com>
2023-07-20 17:25:07 +0800

    fix version

--- service/main.go
+++ service/main.go
@@ -79,7 +79,7 @@ type envStruct struct {
 func main() {
 	app := cli.NewApp()
 	app.Name = "Cassandra-Web"
-	app.Version = "1.1.1"
+	app.Version = "1.1.2"
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name: "Ken",
